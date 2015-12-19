53a79b3ac1b62d0ac817981ba45c0718c85586b2
CarlMcAteer <carlmc12@gmail.com>
2015-12-19 23:22:09 +0000

    first commit

---
+++ ex1.sql
@@ -0,0 +1,8 @@
+CREATE TABLE person (
+	id INTERGER PRIMARY KEY,
+	first_name TEXT,
+	middle)name TEST,
+	last_name TEXT,
+	age INTERGER,
+	height INTERGER
+);
---
+++ ex3.sql
@@ -0,0 +1,13 @@
+INSERT INTO person(id, first_name, last_name, age)
+	VALUES (0, "Zed", "Shaw", 37);
+
+INSERT INTO pet(id, name, breed, age, dead)
+	VALUES (0, "Fluffy", "Unicorn", 1000, 0);
+
+INSERT INTO pet VALUES (1, "Gigantor", "Robot", 1, 1);
+
+INSERT INTO person(id, first_name, last_name, age)
+	VALUES 	(1, "Carl", "McAteer", 24);
+
+INSERT INTO pet(id, name, breed, age, dead)
+	VALUES (2, "Bob", "Cat", 5, 0);
